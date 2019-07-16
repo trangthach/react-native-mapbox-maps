678f06f1b8f1ae890f77f896e0d5399922cb8bd9
Mattijs Fuijkschot <mattijs@innovactory.com>
2019-07-16 09:58:33 +0200

    Migrated lib & example to AndroidX

--- android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/RCTMGLPointAnnotationAdapter.java
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/components/annotation/RCTMGLPointAnnotationAdapter.java
@@ -1,23 +1,5 @@
 package com.mapbox.rctmgl.components.annotation;
 
-import android.content.Context;
-import android.graphics.Bitmap;
-import android.os.Build;
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-
-import com.mapbox.mapboxsdk.annotations.IconFactory;
-import com.mapbox.mapboxsdk.geometry.LatLng;
-import com.mapbox.mapboxsdk.maps.MapboxMap;
-import com.mapbox.rctmgl.R;
-import com.mapbox.rctmgl.components.mapview.RCTMGLMapView;
-
 /**
  * Created by nickitaliano on 9/27/17.
  */
--- android/rctmgl/src/main/java/com/mapbox/rctmgl/modules/RCTMGLLocationModule.java
+++ android/rctmgl/src/main/java/com/mapbox/rctmgl/modules/RCTMGLLocationModule.java
@@ -1,7 +1,7 @@
 package com.mapbox.rctmgl.modules;
 
 import android.location.Location;
-import android.support.annotation.NonNull;
+import androidx.annotation.NonNull;
 
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.Promise;
@@ -12,7 +12,6 @@ import com.facebook.react.modules.core.RCTNativeAppEventEmitter;
 import com.mapbox.android.core.location.LocationEngineCallback;
 import com.mapbox.android.core.location.LocationEngineResult;
 import com.mapbox.rctmgl.events.EventEmitter;
-import com.mapbox.rctmgl.events.IEvent;
 import com.mapbox.rctmgl.events.LocationEvent;
 import com.mapbox.rctmgl.location.LocationManager;
 
